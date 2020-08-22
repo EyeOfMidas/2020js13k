0be8b7f7d0f27f6ea2831824cbf78ea84083f5b3
Justin Gilman <eyeofmidas@gmail.com>
2020-08-22 10:26:08 -0400

    adding auto-install prompt to pack script

--- pack.sh
+++ pack.sh
@@ -3,6 +3,17 @@ rm game.zip
 rm min.js
 rm pack.js
 cat KeyCode.js manualtween.js colors.js dialog.js *-scene.js core.js > pack.js
+
+if ! command -v terser &> /dev/null; then
+	echo "terser not installed!"
+	read -p "Would you like to run the npm global install? [Y/n]:" -n 1 -r
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		sudo npm -g install terser webpack terser-webpack-plugin
+	else
+		echo ""
+		exit 1
+	fi
+fi
 #npm -g install terser webpack terser-webpack-plugin
 terser --compress --output min.js pack.js
 zip game.zip min.js index.html
